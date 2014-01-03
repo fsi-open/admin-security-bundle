96056d5a9cd06bf168832c8e30ae2ac9186545e5
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-01-03 19:13:33 +0100

    Fixed specs

--- spec/FSi/Bundle/AdminSecurityBundle/EventListener/SecuredElementListenerSpec.php
+++ spec/FSi/Bundle/AdminSecurityBundle/EventListener/SecuredElementListenerSpec.php
@@ -9,12 +9,20 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\SecurityContext;
 
 class SecuredElementListenerSpec extends ObjectBehavior
 {
-    function let(Manager $adminManager, SecurityContext $securityContext)
-    {
+    function let(
+        Manager $adminManager,
+        SecurityContext $securityContext,
+        UsernamePasswordToken $token,
+        GetResponseEvent $event
+    ) {
+        $securityContext->getToken()->willReturn($token);
+        $event->getRequestType()->willReturn(Kernel::MASTER_REQUEST);
+
         $this->beConstructedWith($adminManager, $securityContext);
     }
 
@@ -23,9 +31,17 @@ class SecuredElementListenerSpec extends ObjectBehavior
         Manager $adminManager
     ) {
         $event->getRequestType()->shouldBeCalled()->willReturn(Kernel::SUB_REQUEST);
-
         $adminManager->getElements()->shouldNotBeCalled();
+        $this->onKernelRequest($event);
+    }
 
+    function it_do_nothing_when_request_is_made_without_user_token_in_security_context(
+        SecurityContext $securityContext,
+        GetResponseEvent $event,
+        Manager $adminManager
+    ) {
+        $securityContext->getToken()->willReturn(null);
+        $adminManager->getElements()->shouldNotBeCalled();
         $this->onKernelRequest($event);
     }
 
@@ -35,16 +51,14 @@ class SecuredElementListenerSpec extends ObjectBehavior
         SecuredElement $securedElement,
         ElementInterface $element
     ) {
-        $event->getRequestType()->shouldBeCalled()->willReturn(Kernel::MASTER_REQUEST);
-
         $adminManager->getElements()->shouldBeCalled()->willReturn(array(
             $securedElement,
             $element
         ));
 
         $securedElement->isAllowed(
-                Argument::type('Symfony\Component\Security\Core\SecurityContextInterface')
-            )->shouldBeCalled()
+            Argument::type('Symfony\Component\Security\Core\SecurityContext')
+        )->shouldBeCalled()
             ->willReturn(false);
 
         $securedElement->getId()->shouldBeCalled()->willReturn('secured');
