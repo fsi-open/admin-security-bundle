26047b24f93c39421cba5cf4113d3bf9c6d0215b
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2017-04-14 15:34:28 +0200

    Do not send reset password email to disabled user

--- spec/FSi/Bundle/AdminSecurityBundle/Controller/PasswordReset/ResetRequestControllerSpec.php
+++ spec/FSi/Bundle/AdminSecurityBundle/Controller/PasswordReset/ResetRequestControllerSpec.php
@@ -2,7 +2,16 @@
 
 namespace spec\FSi\Bundle\AdminSecurityBundle\Controller\PasswordReset;
 
+use FSi\Bundle\AdminBundle\Message\FlashMessages;
 use FSi\Bundle\AdminSecurityBundle\Event\AdminSecurityEvents;
+use FSi\Bundle\AdminSecurityBundle\Security\User\UserInterface;
+use FSi\Bundle\AdminSecurityBundle\Security\User\UserRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -13,50 +22,19 @@ class ResetRequestControllerSpec extends ObjectBehavior
         $this->shouldHaveType('FSi\Bundle\AdminSecurityBundle\Controller\PasswordReset\ResetRequestController');
     }
 
-    /**
-     * @param \Symfony\Bundle\FrameworkBundle\Templating\EngineInterface $templating
-     * @param \Symfony\Component\Form\FormFactoryInterface $formFactory
-     * @param \Symfony\Component\Routing\RouterInterface $router
-     * @param \FSi\Bundle\AdminSecurityBundle\Security\User\UserRepositoryInterface $userRepository
-     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
-     * @param \FSi\Bundle\AdminBundle\Message\FlashMessages $flashMessages
-     */
-    function let($templating, $formFactory, $router, $userRepository, $eventDispatcher, $flashMessages)
-    {
-        $this->beConstructedWith(
-            $templating,
-            'template_path',
-            $formFactory,
-            $router,
-            $userRepository,
-            $eventDispatcher,
-            $flashMessages,
-            'form_type'
-        );
-    }
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @param \Symfony\Component\Form\FormFactoryInterface $formFactory
-     * @param \Symfony\Component\Form\FormInterface $form
-     * @param \Symfony\Component\Form\FormInterface $form2
-     * @param \FSi\Bundle\AdminSecurityBundle\Security\User\UserRepositoryInterface $userRepository
-     * @param \FSi\Bundle\AdminSecurityBundle\Security\User\UserInterface $user
-     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
-     * @param \Symfony\Component\Routing\RouterInterface $router
-     * @param \FSi\Bundle\AdminBundle\Message\FlashMessages $flashMessages
-     */
-    function it_updates_confirmation_token_and_sends_mail(
-        $request,
-        $formFactory,
-        $form,
-        $form2,
-        $userRepository,
-        $user,
-        $eventDispatcher,
-        $router,
-        $flashMessages
+    function let(
+        EngineInterface $templating,
+        FormFactoryInterface $formFactory,
+        RouterInterface $router,
+        UserRepositoryInterface $userRepository,
+        EventDispatcherInterface $eventDispatcher,
+        FlashMessages $flashMessages,
+        Request $request,
+        FormInterface $form,
+        FormInterface $form2,
+        UserInterface $user
     ) {
+        $user->isEnabled()->willReturn(true);
         $formFactory->create('form_type')->willReturn($form);
         $form->handleRequest($request)->willReturn($form);
         $form->isSubmitted()->willReturn(true);
@@ -70,6 +48,25 @@ class ResetRequestControllerSpec extends ObjectBehavior
         $user->getPasswordResetToken()->willReturn(null);
         $user->isAccountNonLocked()->willReturn(true);
 
+        $this->beConstructedWith(
+            $templating,
+            'template_path',
+            $formFactory,
+            $router,
+            $userRepository,
+            $eventDispatcher,
+            $flashMessages,
+            'form_type'
+        );
+    }
+
+    function it_updates_confirmation_token_and_dispatches_event(
+        Request $request,
+        UserInterface $user,
+        EventDispatcherInterface $eventDispatcher,
+        RouterInterface $router,
+        FlashMessages $flashMessages
+    ) {
         $eventDispatcher->dispatch(
             AdminSecurityEvents::RESET_PASSWORD_REQUEST,
             Argument::allOf(
@@ -77,6 +74,31 @@ class ResetRequestControllerSpec extends ObjectBehavior
                 Argument::which('getUser', $user->getWrappedObject())
             )
         )->shouldBeCalled();
+
+        $flashMessages->info('admin.password_reset.request.mail_sent_if_correct', 'FSiAdminSecurity')->shouldBeCalled();
+
+        $router->generate('fsi_admin_security_user_login')->willReturn('url');
+
+        $response = $this->requestAction($request);
+        $response->shouldHaveType('Symfony\Component\HttpFoundation\RedirectResponse');
+    }
+
+    function it_does_not_dispatch_event_and_displays_warning_when_user_disabled(
+        Request $request,
+        UserInterface $user,
+        EventDispatcherInterface $eventDispatcher,
+        RouterInterface $router,
+        FlashMessages $flashMessages
+    ) {
+        $user->isEnabled()->willReturn(false);
+
+        $eventDispatcher->dispatch(
+            AdminSecurityEvents::RESET_PASSWORD_REQUEST,
+            Argument::allOf(
+                Argument::type('FSi\Bundle\AdminSecurityBundle\Event\ResetPasswordRequestEvent'),
+                Argument::which('getUser', $user->getWrappedObject())
+            )
+        )->shouldNotBeCalled();
 
         $flashMessages->info('admin.password_reset.request.mail_sent_if_correct', 'FSiAdminSecurity')->shouldBeCalled();
 
