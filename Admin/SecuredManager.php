d5df949d046cb8cd31789ec2d9b297474bc3b759
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2017-04-20 16:00:14 +0200

    Replaced RemoveNotGrantedElementsListener with SecuredManager

---
+++ Admin/SecuredManager.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FSi\Bundle\AdminSecurityBundle\Admin;
+
+use FSi\Bundle\AdminBundle\Admin\Element;
+use FSi\Bundle\AdminBundle\Admin\ManagerInterface;
+use FSi\Bundle\AdminBundle\Admin\Manager\Visitor;
+use FSi\Bundle\AdminSecurityBundle\Admin\SecuredElementInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class SecuredManager implements ManagerInterface
+{
+    /**
+     * @var ManagerInterface
+     */
+    private $manager;
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    public function __construct(
+        ManagerInterface $manager,
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
+        $this->manager = $manager;
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addElement(Element $element)
+    {
+        return $this->manager->addElement($element);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasElement($id)
+    {
+        if (!$this->manager->hasElement($id)) {
+            return false;
+        }
+
+        $element = $this->manager->getElement($id);
+        if ($this->isAccessToElementRestricted($element)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $id
+     * @return \FSi\Bundle\AdminBundle\Admin\Element|null
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function getElement($id)
+    {
+        $element = $this->manager->getElement($id);
+        if (!$element) {
+            return null;
+        }
+
+        if ($this->isAccessToElementRestricted($element)) {
+            throw new AccessDeniedException(sprintf(
+                'Access denied to element "%s"',
+                get_class($element)
+            ));
+        }
+
+        return $element;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeElement($id)
+    {
+        $this->manager->removeElement($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getElements()
+    {
+        return array_filter((array) $this->manager->getElements(), function (Element $element) {
+            return !$this->isAccessToElementRestricted($element);
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function accept(Visitor $visitor)
+    {
+        $visitor->visitManager($this);
+    }
+
+    /**
+     * @param Element $element
+     * @return boolean
+     */
+    private function isAccessToElementRestricted(Element $element)
+    {
+        if (!$this->tokenStorage->getToken()) {
+            // The request is not behind a firewall, so all elements are restricted
+            return true;
+        }
+
+        return  $element instanceof SecuredElementInterface
+            && !$element->isAllowed($this->authorizationChecker)
+        ;
+    }
+}
---
+++ spec/FSi/Bundle/AdminSecurityBundle/Admin/SecuredManagerSpec.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\FSi\Bundle\AdminSecurityBundle\Admin;
+
+use FSi\Bundle\AdminBundle\Admin\Element;
+use FSi\Bundle\AdminBundle\Admin\ManagerInterface;
+use FSi\Bundle\AdminBundle\Admin\Manager\Visitor;
+use FSi\Bundle\AdminSecurityBundle\spec\fixtures\SecuredElement;
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SecuredManagerSpec extends ObjectBehavior
+{
+    const INSECURE_ID = 'insecure';
+    const SECURE_ID = 'secure';
+
+    function let(
+        AuthorizationCheckerInterface $authorizationChecker,
+        ManagerInterface $manager,
+        Element $insecureElement,
+        SecuredElement $securedElement
+    ) {
+        $insecureElement->getId()->willReturn(self::INSECURE_ID);
+        $securedElement->getId()->willReturn(self::SECURE_ID);
+        $manager->hasElement(self::INSECURE_ID)->willReturn(true);
+        $manager->hasElement(self::SECURE_ID)->willReturn(true);
+        $manager->getElement(self::INSECURE_ID)->willReturn($insecureElement);
+        $manager->getElement(self::SECURE_ID)->willReturn($securedElement);
+
+        $this->beConstructedWith($manager, $authorizationChecker);
+    }
+
+    function it_implements_manager_interface()
+    {
+        $this->beAnInstanceOf('\FSi\Bundle\AdminBundle\Admin\ManagerInterface');
+    }
+
+    function it_adds_element_regardless_of_access(
+        ManagerInterface $manager,
+        AuthorizationCheckerInterface $authorizationChecker,
+        Element $insecureElement,
+        SecuredElement $securedElement
+    ) {
+        $manager->addElement($insecureElement)->shouldBeCalled();
+        $this->addElement($insecureElement);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(false);
+        $manager->addElement($securedElement)->shouldBeCalled();
+        $this->addElement($securedElement);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(true);
+        $manager->addElement($securedElement)->shouldBeCalled();
+        $this->addElement($securedElement);
+    }
+
+    function it_removes_element_by_id_regardless_of_access(
+        ManagerInterface $manager,
+        AuthorizationCheckerInterface $authorizationChecker,
+        SecuredElement $securedElement
+    ) {
+        $manager->removeElement(self::INSECURE_ID)->shouldBeCalled();
+        $this->removeElement(self::INSECURE_ID);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(false);
+        $manager->removeElement(self::SECURE_ID)->shouldBeCalled();
+        $this->removeElement(self::SECURE_ID);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(true);
+        $manager->removeElement(self::SECURE_ID)->shouldBeCalled();
+        $this->removeElement(self::SECURE_ID);
+    }
+
+    function it_returns_true_for_has_element_if_access_allowed(
+        AuthorizationCheckerInterface $authorizationChecker,
+        SecuredElement $securedElement
+    ) {
+        $this->hasElement(self::INSECURE_ID)->shouldReturn(true);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(true);
+        $this->hasElement(self::SECURE_ID)->shouldReturn(true);
+    }
+
+    function it_returns_false_for_has_element_if_access_not_allowed(
+        AuthorizationCheckerInterface $authorizationChecker,
+        SecuredElement $securedElement
+    ) {
+        $this->hasElement(self::INSECURE_ID)->shouldReturn(true);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(false);
+        $this->hasElement(self::SECURE_ID)->shouldReturn(false);
+    }
+
+    function it_throws_exception_when_trying_to_get_a_disallowed_element(
+        AuthorizationCheckerInterface $authorizationChecker,
+        SecuredElement $securedElement
+    ) {
+        $securedElement->isAllowed($authorizationChecker)->willReturn(false);
+
+        $this->shouldThrow(
+            '\Symfony\Component\Security\Core\Exception\AccessDeniedException'
+        )->during('getElement', [self::SECURE_ID]);
+    }
+
+    function it_returns_only_allowed_elements(
+        ManagerInterface $manager,
+        AuthorizationCheckerInterface $authorizationChecker,
+        Element $insecureElement,
+        SecuredElement $securedElement
+    ) {
+        $manager->getElements()->willReturn([$insecureElement, $securedElement]);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(false);
+        $this->getElements()->shouldReturn([$insecureElement]);
+
+        $securedElement->isAllowed($authorizationChecker)->willReturn(true);
+        $this->getElements()->shouldReturn([$insecureElement, $securedElement]);
+    }
+
+    function it_accepts_visitors(ManagerInterface $manager, Visitor $visitor)
+    {
+        $manager->accept($visitor)->shouldBeCalled();
+        $this->accept($visitor);
+    }
+}
