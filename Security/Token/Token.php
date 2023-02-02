60c41a22b07153cb34075978d93c561e209b530b
Piotr Szymaszek <pszymaszek@gmail.com>
2023-02-02 10:15:29 +0100

    Removed nullable type from fields Token

--- Security/Token/Token.php
+++ Security/Token/Token.php
@@ -11,15 +11,14 @@ declare(strict_types=1);
 
 namespace FSi\Bundle\AdminSecurityBundle\Security\Token;
 
-use Assert\Assertion;
 use DateInterval;
 use DateTimeImmutable;
 
 class Token implements TokenInterface
 {
-    private ?string $token = null;
-    private ?DateTimeImmutable $createdAt = null;
-    private ?DateTimeImmutable $expiresAt = null;
+    private string $token;
+    private DateTimeImmutable $createdAt;
+    private DateTimeImmutable $expiresAt;
 
     public function __construct(string $token, DateTimeImmutable $createdAt, DateInterval $ttl)
     {
@@ -30,25 +29,21 @@ class Token implements TokenInterface
 
     public function getCreatedAt(): DateTimeImmutable
     {
-        Assertion::notNull($this->createdAt);
         return $this->createdAt;
     }
 
     public function getToken(): string
     {
-        Assertion::notNull($this->token);
         return $this->token;
     }
 
     public function getExpiresAt(): DateTimeImmutable
     {
-        Assertion::notNull($this->expiresAt);
         return $this->expiresAt;
     }
 
     public function isNonExpired(): bool
     {
-        Assertion::notNull($this->expiresAt);
         return new DateTimeImmutable() <= $this->expiresAt;
     }
 }
